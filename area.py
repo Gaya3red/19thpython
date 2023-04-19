384c21d05bfe8ab0d1015e4d21bdac653370880b
Siddamurthy Gayathri <gaya3reddysiddamurthy223@gmail.com>
2023-04-19 12:37:24 +0530

    update

---
+++ area.py
@@ -0,0 +1,11 @@
+b=int(input("Enter base of the triangle:"))
+h=int(input("Enter height of the triangle:"))
+area=0.5*b*h
+print("Area of triangle is",area)
+l=int(input("Enter length :"))
+a=l*l
+print("Area of square is",a)
+lg=int(input("Enter length :"))
+bd=int(input("Enter breadth :"))
+b=lg*bd
+print("Area of rectangle is",b)
