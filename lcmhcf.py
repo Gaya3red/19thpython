1fcceeb03060c6a97f9e46015e8de116952ff8ac
Siddamurthy Gayathri <gaya3reddysiddamurthy223@gmail.com>
2023-04-24 12:15:25 +0530

    update

---
+++ lcmhcf.py
@@ -0,0 +1,12 @@
+def findLcm(a,b):
+    if a>b:
+        greater =a
+    else:
+        greater= b
+    while True:
+        if((greater % a==0)and (greater%b==0)):
+            lcm = greater
+            break
+        greater+=1
+    return lcm
+print(findLcm(10,20))
