63acc8e0f2a0d1f2823b06dbc525d75ec71acd52
Siddamurthy Gayathri <gaya3reddysiddamurthy223@gmail.com>
2023-04-25 12:33:34 +0530

    update

---
+++ dict.py
@@ -0,0 +1,7 @@
+my_dict = {
+    "name":"Gaya3",
+    "age":20,
+    "Occupation":"Student"
+}
+print(type(my_dict))
+print(my_dict["name"])
---
+++ dictupdatemethod.py
@@ -0,0 +1,12 @@
+my_dict = {
+    "name":"Gaya3",
+    "age":20,
+    "Occupation":"Student"
+}
+print(type(my_dict))
+print(my_dict["name"])
+update_name = {
+    "name" : "Gayathri"
+}
+my_dict.update(update_name)
+print(my_dict["name"])
